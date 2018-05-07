ee80759ac781a398c4e59098a347653d55668ac7
uceshun <uceshun@ucl.ac.uk>
2018-05-07 08:06:52 -0400

    for week 6

---
+++ uceshun/www/js/appActivity.js
@@ -0,0 +1,152 @@
+// the variables
+// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on 
+var earthquakelayer;
+// a global variable to hold the http request
+var client;
+// store the map
+var mymap;
+
+var testMarkerRed = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'red'
+});
+
+var testMarkerPink = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'pink'
+});
+
+// this is the code that runs when the App starts
+
+	loadMap();
+	//showPointLineCircle();
+	
+		
+		
+// ***********************************
+// the functions
+
+function trackLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+		alert("geolocation is not supported by this browser");
+    }
+}
+function showPosition(position) {
+	// draw a point on the map
+	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
+		.bindPopup("<b>You were at "+ position.coords.longitude + " "+position.coords.latitude+"!</b>");
+	mymap.setView([position.coords.latitude, position.coords.longitude], 13);
+}
+
+
+function loadMap(){
+		mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+			// load the tiles
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(mymap);
+
+}
+
+
+		function showPointLineCircle(){
+				// add a point
+	L.marker([51.5, -0.09]).addTo(mymap)
+		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+	// add a circle
+	L.circle([51.508, -0.11], 500, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap).bindPopup("I am a circle.");
+	// add a polygon with 3 end points (i.e. a triangle)
+	var myPolygon = L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	],{
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap).bindPopup("I am a polygon.");
+
+		}
+		
+		
+// call the server
+function getEarthquakes() {
+   // set up the request
+   client = new XMLHttpRequest();
+   // make the request to the URL
+   client.open('GET','http://developer.cege.ucl.ac.uk:30261/getGeoJSON/united_kingdom_highway/geom');
+   // tell the request what method to run that will listen for the response
+   client.onreadystatechange = earthquakeResponse;  // note don't use earthquakeResponse() with brackets as that doesn't work
+   // activate the request
+   client.send();
+}
+// receive the response
+function earthquakeResponse() {
+  // wait for a response - if readyState is not 4 then keep waiting 
+  if (client.readyState == 4) {
+    // get the data from the response
+    var earthquakedata = client.responseText;
+    // call a function that does something with the data
+    loadearthquakelayer(earthquakedata);
+  }
+}
+function loadearthquakelayer(earthquakedata) {
+      // convert the text received from the server to JSON 
+      var earthquakejson = JSON.parse(earthquakedata );
+
+      // load the geoJSON layer
+      var earthquakelayer = L.geoJson(earthquakejson,
+        {
+            // use point to layer to create the points
+            pointToLayer: function (feature, latlng)
+            {
+              // look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
+              // also include a pop-up that shows the place value of the earthquakes
+              if (feature.properties.mag > 1.75) {
+                 return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.place +"</b>");
+              }
+              else {
+                // magnitude is 1.75 or less
+                return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.place +"</b>");;
+              }
+            },
+        }).addTo(mymap); 
+    mymap.fitBounds(earthquakelayer.getBounds());
+}
+
+
+//*************************
+// functions to change the DIV content using AJAX - week 5
+
+var  xhr;  // define the global variable to process the AJAX request
+function callDivChange() {
+
+	 xhr = new XMLHttpRequest();
+
+	 // use an HTTP request here as Edge doesn't work with HTTPS over express
+	 xhr.open('GET', 'http://developer.cege.ucl.ac.uk:30261/dir1/dir2/objectTest5.html');
+	 xhr.onreadystatechange = processDivChange;
+	xhr.send();
+}  
+function processDivChange() {
+if (xhr.readyState < 4)    {}                     // while waiting response from server
+        //document.getElementById('ajaxtest').innerHTML = "Loading...";
+
+    else {
+		if (xhr.readyState === 4) {               // 4 = Response from server has been completely loaded.
+            document.getElementById('ajaxtest').innerHTML = xhr.responseText;
+    }
+}
+}
